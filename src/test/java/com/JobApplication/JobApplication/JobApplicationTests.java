68a820b1f992b83e3827d923af776abec65b1966
WinHome Test <144950366+Randomlyclueless@users.noreply.github.com>
2026-07-01 18:16:18 +0530

    Initial Spring Boot Job Application project

---
+++ src/test/java/com/JobApplication/JobApplication/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobApplication.JobApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
